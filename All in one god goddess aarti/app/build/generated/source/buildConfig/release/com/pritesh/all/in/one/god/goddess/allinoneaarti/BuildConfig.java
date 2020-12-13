40b98a5d3aba4b7076d2e68f3bd0fdc80c881d0f
Pritesh Parmar <pritesh4333>
2020-12-13 19:56:29 +0530

    master instagram reels project add

---
+++ All in one god goddess aarti/app/build/generated/source/buildConfig/release/com/pritesh/all/in/one/god/goddess/allinoneaarti/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.pritesh.all.in.one.god.goddess.allinoneaarti;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.pritesh.all.in.one.god.goddess.allinoneaarti";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 5;
+  public static final String VERSION_NAME = "1.5";
+}
---
+++ instagram reels/app/build/generated/source/buildConfig/debug/com/reels/video/download/instagram/video/downloader/saver/story/video/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.reels.video.download.instagram.video.downloader.saver.story.video;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.reels.video.download.instagram.video.downloader.saver.story.video";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
